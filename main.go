99883ead1b606ee369b388df38691466e213b7c2
kalebE36 <kaleb.ellington33@gmail.com>
2024-09-28 18:18:24 -0400

    nuked auth0

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-
 import (
 	"encoding/gob"
 	"log"
 	"net/http"
+	"time"
 
 	"SHELLHACKS-BACKEND/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -29,6 +30,15 @@ func main() {
 
 	// Create a Gin router
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4321"}, // Add localhost or other allowed origins here
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-Length"},
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Define routes using your existing routes package
 	router.GET("/callback", routes.CallbackHandler())
